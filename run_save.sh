89d45984d798679e5dec41a6237ebfdd96285fa2
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-10-08 13:17:08 +0200

    feat: allow for benchmarking and plotting seq length

--- run_save.sh
+++ run_save.sh
@@ -31,97 +31,101 @@ echo "Making solutions"
 make all > /dev/null
 
 # Define an array of sample sizes
-SAMPLE_SIZES=(5000 10000 100000 200000 300000)  # Modify as needed
-SEQ_LENGTH=10000
+SAMPLE_SIZES=(50000)  # Modify as needed
+SEQ_LENGTHS=(500 1000 5000 10000) # Modify as needed
 RESULTS_FILE="benchmarks/timing_results.txt"
 
 # Initialize the results file
-echo "Sample Size,Sequence length,CPU Time (ms),CPU Hashtable Time (ms),GPU Time (ms),Optimized GPU Time (ms),GPU Hashtable Time (ms)" > $RESULTS_FILE
+echo "Sample Size,Sequence Length,CPU Time (ms),CPU Hashtable Time (ms),GPU Time (ms),Optimized GPU Time (ms),GPU Hashtable Time (ms)" > $RESULTS_FILE
 
 # Loop over each sample size
-for SAMPLE_SIZE in "${SAMPLE_SIZES[@]}"
+for SEQ_LENGTH in "${SEQ_LENGTHS[@]}"
 do
-    echo "Running tests with sample size: $SAMPLE_SIZE"
+    echo "Running tests with sequence length: $SEQ_LENGTH"
+    for SAMPLE_SIZE in "${SAMPLE_SIZES[@]}"
+    do
+        echo "Running tests with sample size: $SAMPLE_SIZE"
 
-    # Generate input if --no-gen is not specified
-    if [ "$GEN_INPUT" = true ]; then
-        echo "Generating input"
-        ./bin/generate_input input/input.txt "$SEQ_LENGTH" "$SAMPLE_SIZE"
-    else
-        echo "Skipping input generation"
-    fi
+        # Generate input if --no-gen is not specified
+        if [ "$GEN_INPUT" = true ]; then
+            echo "Generating input"
+            ./bin/generate_input input/input.txt "$SEQ_LENGTH" "$SAMPLE_SIZE"
+        else
+            echo "Skipping input generation"
+        fi
 
-    # Initialize timing variables
-    CPU_TIME=0
-    CPU_HASHTABLE_TIME=0
-    GPU_TIME=0
-    OPT_GPU_TIME=0
-    HASH_GPU_TIME=0
+        # Initialize timing variables
+        CPU_TIME=0
+        CPU_HASHTABLE_TIME=0
+        GPU_TIME=0
+        OPT_GPU_TIME=0
+        HASH_GPU_TIME=0
 
-    # Run CPU if --no-cpu is not specified
-    if [ "$RUN_CPU" = true ]; then
-        echo "Running CPU"
-        CPU_START_TIME=$(date +%s%N | cut -b1-13)
-        ./bin/hamming_one_cpu input/input.txt > output/output1.txt
-        CPU_END_TIME=$(date +%s%N | cut -b1-13)
-        CPU_TIME=$(($CPU_END_TIME - $CPU_START_TIME))
+        # Run CPU if --no-cpu is not specified
+        if [ "$RUN_CPU" = true ]; then
+            echo "Running CPU"
+            CPU_START_TIME=$(date +%s%N | cut -b1-13)
+            ./bin/hamming_one_cpu input/input.txt > output/output1.txt
+            CPU_END_TIME=$(date +%s%N | cut -b1-13)
+            CPU_TIME=$(($CPU_END_TIME - $CPU_START_TIME))
 
-        echo "Running CPU with hashtable"
-        CPU_HASHTABLE_START_TIME=$(date +%s%N | cut -b1-13)
-        ./bin/hamming_one_hashtable_cpu input/input.txt > output/output4.txt
-        CPU_HASHTABLE_END_TIME=$(date +%s%N | cut -b1-13)
-        CPU_HASHTABLE_TIME=$(($CPU_HASHTABLE_END_TIME - $CPU_HASHTABLE_START_TIME))
-    else
-        echo "Skipping CPU run, using GPU output as baseline"
-    fi
+            echo "Running CPU with hashtable"
+            CPU_HASHTABLE_START_TIME=$(date +%s%N | cut -b1-13)
+            ./bin/hamming_one_hashtable_cpu input/input.txt > output/output4.txt
+            CPU_HASHTABLE_END_TIME=$(date +%s%N | cut -b1-13)
+            CPU_HASHTABLE_TIME=$(($CPU_HASHTABLE_END_TIME - $CPU_HASHTABLE_START_TIME))
+        else
+            echo "Skipping CPU run, using GPU output as baseline"
+        fi
 
-    # Run GPU
-    echo "Running GPU"
-    GPU_START_TIME=$(date +%s%N | cut -b1-13)
-    ./bin/hamming_one_gpu input/input.txt > output/output2.txt
-    GPU_END_TIME=$(date +%s%N | cut -b1-13)
-    GPU_TIME=$(($GPU_END_TIME - $GPU_START_TIME))
+        # Run GPU
+        echo "Running GPU"
+        GPU_START_TIME=$(date +%s%N | cut -b1-13)
+        ./bin/hamming_one_gpu input/input.txt > output/output2.txt
+        GPU_END_TIME=$(date +%s%N | cut -b1-13)
+        GPU_TIME=$(($GPU_END_TIME - $GPU_START_TIME))
 
-    # If CPU run was skipped, use GPU output as baseline
-    if [ "$RUN_CPU" = false ]; then
-        cp output/output2.txt output/output1.txt
-    fi
+        # If CPU run was skipped, use GPU output as baseline
+        if [ "$RUN_CPU" = false ]; then
+            cp output/output2.txt output/output1.txt
+        fi
 
-    # Run Optimized GPU
-    echo "Running optimised GPU"
-    OPT_GPU_START_TIME=$(date +%s%N | cut -b1-13)
-    ./bin/hamming_one_optimised_gpu input/input.txt > output/output3.txt
-    OPT_GPU_END_TIME=$(date +%s%N | cut -b1-13)
-    OPT_GPU_TIME=$(($OPT_GPU_END_TIME - $OPT_GPU_START_TIME))
+        # Run Optimized GPU
+        echo "Running optimised GPU"
+        OPT_GPU_START_TIME=$(date +%s%N | cut -b1-13)
+        ./bin/hamming_one_optimised_gpu input/input.txt > output/output3.txt
+        OPT_GPU_END_TIME=$(date +%s%N | cut -b1-13)
+        OPT_GPU_TIME=$(($OPT_GPU_END_TIME - $OPT_GPU_START_TIME))
 
-    # Run HashTable GPU
-    if [ "$RUN_GPU_HASH" = true ]; then
-        echo "Running GPU with hashtable"
-        HASH_GPU_START_TIME=$(date +%s%N | cut -b1-13)
-        ./bin/hamming_one_hashtable_gpu input/input.txt > output/output5.txt
-        HASH_GPU_END_TIME=$(date +%s%N | cut -b1-13)
-        HASH_GPU_TIME=$(($HASH_GPU_END_TIME - $HASH_GPU_START_TIME))
-    fi
+        # Run HashTable GPU
+        if [ "$RUN_GPU_HASH" = true ]; then
+            echo "Running GPU with hashtable"
+            HASH_GPU_START_TIME=$(date +%s%N | cut -b1-13)
+            ./bin/hamming_one_hashtable_gpu input/input.txt > output/output5.txt
+            HASH_GPU_END_TIME=$(date +%s%N | cut -b1-13)
+            HASH_GPU_TIME=$(($HASH_GPU_END_TIME - $HASH_GPU_START_TIME))
+        fi
 
-    # Save results to the text file
-    if [ "$RUN_CPU" = true ]; then
-        echo "$SAMPLE_SIZE,$SEQ_LENGTH,$CPU_TIME,$CPU_HASHTABLE_TIME,$GPU_TIME,$OPT_GPU_TIME,$HASH_GPU_TIME" >> $RESULTS_FILE
-    else
-        echo "$SAMPLE_SIZE,$SEQ_LENGTH,$CPU_TIME,$CPU_HASHTABLE_TIME,$GPU_TIME,$OPT_GPU_TIME" >> $RESULTS_FILE
-    fi
+        # Save results to the text file
+        if [ "$RUN_CPU" = true ]; then
+            echo "$SAMPLE_SIZE,$SEQ_LENGTH,$CPU_TIME,$CPU_HASHTABLE_TIME,$GPU_TIME,$OPT_GPU_TIME,$HASH_GPU_TIME" >> $RESULTS_FILE
+        else
+            echo "$SAMPLE_SIZE,$SEQ_LENGTH,$CPU_TIME,$CPU_HASHTABLE_TIME,$GPU_TIME,$OPT_GPU_TIME" >> $RESULTS_FILE
+        fi
 
-    # Output results to the terminal
-    echo "Results for sample size $SAMPLE_SIZE:"
-    if [ "$RUN_CPU" = true ]; then
-        echo "CPU calculations took $CPU_TIME milliseconds to complete"
-        echo "CPU with hashtable calculations took $CPU_HASHTABLE_TIME milliseconds to complete"
-    fi
-    echo "GPU calculations took $GPU_TIME milliseconds to complete"
-    echo "Optimised GPU calculations took $OPT_GPU_TIME milliseconds to complete"
-    if [ "$RUN_GPU_HASH" = true ]; then
-        echo "GPU with hashtable calculations took $HASH_GPU_TIME milliseconds to complete"
-    fi
+        # Output results to the terminal
+        echo "Results for sample size $SAMPLE_SIZE:"
+        if [ "$RUN_CPU" = true ]; then
+            echo "CPU calculations took $CPU_TIME milliseconds to complete"
+            echo "CPU with hashtable calculations took $CPU_HASHTABLE_TIME milliseconds to complete"
+        fi
+        echo "GPU calculations took $GPU_TIME milliseconds to complete"
+        echo "Optimised GPU calculations took $OPT_GPU_TIME milliseconds to complete"
+        if [ "$RUN_GPU_HASH" = true ]; then
+            echo "GPU with hashtable calculations took $HASH_GPU_TIME milliseconds to complete"
+        fi
 
+    done
 done
 
 echo "All tests completed. Results saved in $RESULTS_FILE."
--- utils/plot_timing.py
+++ utils/plot_timing.py
@@ -1,29 +1,39 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description="Plot the timing results of the benchmarks")
+parser.add_argument("--seq-length", action='store_true', help="Plot with seq length on X-axis", default=False)
+args = parser.parse_args()
 
 data = pd.read_csv(os.path.join("benchmarks", "timing_results.txt"))
 
 sample_sizes = data["Sample Size"]
-seq_length = data["Sequence Length"]
+seq_lengths = data["Sequence Length"]
 cpu_times = data["CPU Time (ms)"]
 cpu_hashtable_times = data["CPU Hashtable Time (ms)"]
 gpu_times = data["GPU Time (ms)"]
 opt_gpu_times = data["Optimized GPU Time (ms)"]
 gpu_hashtable_times = data["GPU Hashtable Time (ms)"]
 
-plt.figure(figsize=(10, 6))
-plt.plot(sample_sizes, cpu_times, label="CPU", marker='o')
-plt.plot(sample_sizes, cpu_hashtable_times, label="CPU with Hashtable", marker='o')
-plt.plot(sample_sizes, gpu_times, label="GPU", marker='o')
-plt.plot(sample_sizes, opt_gpu_times, label="Optimized GPU", marker='o')
-plt.plot(sample_sizes, gpu_hashtable_times, label="GPU with Hashtable", marker='o')
+x_ticks = seq_lengths if args.seq_length else sample_sizes
+x_label = "Sequence Length" if args.seq_length else "Number of Samples"
+title = "Performance of Different Implementations by Sequence Length" if args.seq_length \
+   else "Performance of Different Implementations by Sample Size"
 
-plt.xlabel("Number of Samples")
+plt.figure(figsize=(10, 6))
+plt.plot(x_ticks, cpu_times, label="CPU", marker='o')
+plt.plot(x_ticks, cpu_hashtable_times, label="CPU with Hashtable", marker='o')
+plt.plot(x_ticks, gpu_times, label="GPU", marker='o')
+plt.plot(x_ticks, opt_gpu_times, label="Optimized GPU", marker='o')
+plt.plot(x_ticks, gpu_hashtable_times, label="GPU with Hashtable", marker='o')
+
+plt.xlabel(x_label)
 plt.ylabel("Time (ms)")
-plt.title("Performance of Different Implementations by Sample Size")
+plt.title(title)
 plt.legend()
 plt.grid(True)
 plt.yscale("log")
 
-plt.savefig(os.path.join("plots", f"performance_plot_{sample_sizes[0]}_{seq_length[0]}.png"))
+plt.savefig(os.path.join("plots", f"performance_plot_{sample_sizes[0]}_{seq_lengths[0]}.png"))
